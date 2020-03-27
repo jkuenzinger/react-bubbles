6138cc99776deb303b3a9ef748dd068e850bf897
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-27 10:02:30 -0700

    study questions answered and added to a question.txt file in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
 import PrivateRoute from './components/PrivateRoute';
 import BubblePage from './components/BubblePage';
-import Login from "./components/Login";
+
 import "./styles.scss";
 
 function App() {
